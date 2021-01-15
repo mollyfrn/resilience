40be23db5f68fe2783924622933b4e69ddea55e2
Molly <mollyfran93@gmail.com>
2021-01-15 15:00:27 -0500

    init cleaning script for database

---
+++ epicndata_cleaningscript.R
@@ -0,0 +1,16 @@
+#Database wrangling and cleanup for EPIC-N database 
+#Molly F. Jenkins 
+#01/15/2021
+
+#This script takes a raw database and cleans it of excess commas and carriage returns, 
+#rendering it usable for analyses. 
+
+####Pre-requisites####
+#install packages & add to library 
+
+install.packages("tidyverse")
+library(tidyverse)
+
+#download link to data 
+#epicN <- read.txt()
+
