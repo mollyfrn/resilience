a9cb7bfe6740f1a18aae276f919fa46cc99142da
Molly <mollyfran93@gmail.com>
2021-01-15 14:14:42 -0500

    updated code with advanced filtering and mutate functions
    additional code paths forward below the hashtags

--- resilience_coding.R
+++ resilience_coding.R
@@ -1,6 +1,6 @@
 ####10.5.5 Resilience Storymapping Project: Pilot Script####
 #Molly F Jenkins 
-#01/11/2020 
+#01/11/2021
 
 #This script takes a database of EPIC-N projects and systematically categorizes them 
 #according to an EPA-developed resilience indicator framework, 
@@ -17,33 +17,33 @@ case_studies <- read.csv("addresses_raw.txt") #database file
 resilience_keywords <- read.csv("resilience+indicators.csv") #resilience keyword file 
 
 #take database, comb for matches with each resilience search term, 
-#assigning an indicator and domain category to each match 
-cases = case_studies$projectName 
-for(c in cases) { 
-  }
-#bas
-tidy_epicn = case_studies %>% 
-             select(contains())
+#assigning an indicator and domain category to each match
+
+#I think this is the best right track right now? no loop necessary? 
+mini_data = case_studies %>% 
+  filter(ProjectName %in% resilience_keywords$suggestedsupplementalsearchterms |
+           ProjectAbstract %in% resilience_keywords$suggestedsupplementalsearchterms | 
+           ProjectDescript %in% resilience_keywords$suggestedsupplementalsearchterms) %>% 
+  mutate(category == resilience_keywords$searchcategory) %>% 
+  select(everything)
+
+mini_data
 
 ######code snippets####
+
+
+
+
+cases = case_studies$projectName 
+for(c in cases) { 
+}
+#bas
+tidy_epicn = case_studies %>% 
+  select(contains())
 
 df_loop2 = df_loop %>%
   group_by(stakeholder) %>%
   mutate(key_word = case_when(
-    stakeholder == "teach" ~ c("teaching", "education", "learning"),
-    stakeholder == "plan" ~ c("planning", "assessment", "transportation"),
-    stakeholder == "policy" ~ c("policy", "regulation", "compensatory"),
-    stakeholder == "health" ~ c("health", "pollution", "exercise"),
-    stakeholder == "water" ~ c("water", "wetland", "hydrologic"),
-    stakeholder == "restore" ~ c("restoration", "mitigation", "apply"),
-    stakeholder == "market" ~ c("market", "credit", "business"),
-    stakeholder == "urban" ~ c("urban", "development", "infrastructure"),
-    stakeholder == "ag" ~ c("ag", "rural", "farm"),
-    stakeholder == "research" ~ c("analysis", "academic", "dissertation"),
-    stakeholder == "GIS" ~ c("GIS", "mapping", "technology"),
-    stakeholder == "parks" ~ c("facilities", "parks", "recreation"),
-    stakeholder == "utilities" ~ c("utilities", "stormwater", "management"),
+    EPA_indicator == EPA_indictor ~ suggested_supplemental_search_terms,
     stakeholder == "council_mayor" ~ c("council", "mayor", "municipal"),
     TRUE ~ "everything_else"))
-
-  
