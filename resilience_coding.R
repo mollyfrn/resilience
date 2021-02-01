d46b03262461a82871d9b30ce552a77be198a525
Molly <mollyfran93@gmail.com>
2021-02-01 15:38:48 -0500

    for loop works need to optimize string search with wildcard operator

--- resilience_coding.R
+++ resilience_coding.R
@@ -64,7 +64,8 @@ for(c in categories){
 }
 
 write.csv(df_final, "Categorized_case_studies_EPICN.csv", row.names = FALSE)
-#code "works" but I don't think it's actually parsing correctly - need to use stringr functions in lieu of %in%
+
+#code works but need to account for * operator for search terms to optimize search 
 
 ######code snippets####
 
